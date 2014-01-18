c02849ddd725493dc520aa8efc74c74f652fbf51
Rajesh Rathod <rajesh.13kits@gmail.com>
2014-01-18 16:50:10 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JiraMetrics::Application.config.secret_key_base = '9a0e9379e12cd01165c1b31ef6fcd714f64d87c72241eedb9a33fe2224fa1f5ea19fff930731a7ac8ecee0e5ae794210af57e59efa544c7dc1f7351b669c456c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JiraMetrics::Application.config.session_store :cookie_store, key: '_jira_metrics_session'
