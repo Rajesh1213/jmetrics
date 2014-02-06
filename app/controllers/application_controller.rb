897da676f98beb33cbc2cf90d3de711c918ef703
Rajesh Rathod <rajesh.13kits@gmail.com>
2014-02-07 01:00:25 +0530

    central module for get/post requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  # require '../../lib/restclientjira.rb'
+  include RestClientJira
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def post_search(sub_url,payload)
+    response = RestClientJira.post(sub_url, payload)
+  end
 
-  
+  def get_search(sub_url)
+    response = RestClientJira.get(sub_url)	  
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module JiraMetrics
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.autoload_paths += %W(#{config.root}/lib)
+    
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -20,5 +24,6 @@ module JiraMetrics
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
