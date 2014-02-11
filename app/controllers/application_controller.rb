2759ef124612cffd49d0dc7baa34a5deb364c485
Rajesh <rajesh.banoth@seneca-global.com>
2014-02-11 20:24:26 +0530

    Tables added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   # require '../../lib/restclientjira.rb'
-  include RestClientJira
+  # include RestClientJira
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
   def post_search(sub_url,payload)
     response = RestClientJira.post(sub_url, payload)
   end
