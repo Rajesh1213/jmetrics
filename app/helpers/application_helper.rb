9d6264ccab1edad70c5dc94365356552ff51e40f
Rajesh Rathod <rajesh.13kits@gmail.com>
2014-02-26 01:00:38 +0530

    Work Request Complexity table is now dymnamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def get_request_count(type,array)
+    count = 0
+    array.each do |ele|
+      count = count.next if ele.eql?(type)
+    end
+    count
+  end
+
+  def percent_of(val1, val2)
+  	"#{((val1.to_f/val2.to_f)*100).round(2)} %"
+  end
+
 end
